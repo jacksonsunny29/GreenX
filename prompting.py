0d1225be493f6d30db849e8e1804a6fce3dab130
Gautam <1gautamj@gmail.com>
2023-09-16 17:14:37 +0100

    use turbo gpt instead of da vinci

--- prompting.py
+++ prompting.py
@@ -1,4 +1,5 @@
 import os
+from pyexpat.errors import messages
 import openai
 from dotenv import load_dotenv
 import json
@@ -9,7 +10,7 @@ class TextAnalyser(object):
 
     openai.api_key = os.getenv("OPENAIKEY")
 
-    keywords = [line.strip() for line in open('keywords.txt', 'r')]
+    keywords = [line.strip() for line in open('keywords.csv', 'r')]
 
     def __init__(self, inText) -> None:
         self.text = inText
@@ -24,11 +25,22 @@ class TextAnalyser(object):
         )
 
         # Request sentiment analysis from GPT-3 in one API call
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=self.sentiPrompt,
-            max_tokens=100  # Adjust as needed
+        # response = openai.Completion.create(
+        #     engine="text-davinci-002",
+        #     prompt=self.sentiPrompt,
+        #     max_tokens=100  # Adjust as needed
+        # )
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": self.sentiPrompt}
+            ]
         )
+
         # Extract and structure the sentiment analysis for each keyword
-        sentiments = json.loads(response["choices"][0]["text"])
+        # sentiments = json.loads(response["choices"][0]["text"])
+        sentiments = json.loads(completion["choices"][0]["message"]["content"])
         return sentiments
+    
+
+
