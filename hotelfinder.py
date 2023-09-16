7abd1a7beaf5b69ca7795fde0ff2c7616466784d
Gautam <1gautamj@gmail.com>
2023-09-17 00:53:48 +0100

    add sustainability heuristics

--- hotelfinder.py
+++ hotelfinder.py
@@ -8,7 +8,7 @@ class HotelFinder(object):
     def __init__(self, inText) -> None:
         self.topicWeights = np.array(list(TextAnalyser(inText=inText).getSents().values()))
         # could need transpositions
-        self.scores = np.dot(hotelsMatrix, self.topicWeights)
+        self.scores = np.dot(hotelsMatrix, self.topicWeights) + np.array([h[2] for h in hotelsID])
 
     def getNBestScorers(self, n):
         indeces = np.argsort(-self.scores)[:n]
