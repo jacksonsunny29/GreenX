78b06c5f7bd76144203e71eaa3ecbadb7c9dba1a
SaturneV <75544574+SaturneV@users.noreply.github.com>
2023-09-16 23:43:20 +0200

    Added main.py and hotelRanker.py

--- hotelfinder.py
+++ hotelfinder.py
@@ -1,15 +1,15 @@
 from prompting import TextAnalyser
 import numpy as np
+from hotelRanker import hotelsID, hotelsMatrix
+
 
 class HotelFinder(object):
-    
-    hotelMatrix, hotelIds = ([], [])
 
     def __init__(self, inText) -> None:
-        self.topicWeights = np.array(TextAnalyser(inText=inText).getSents().values())
-        # could need transpoisitions
-        self.scores = np.matmul(HotelFinder.hotelMatrix, self.topicWeights)
+        self.topicWeights = np.array(list(TextAnalyser(inText=inText).getSents().values()))
+        # could need transpositions
+        self.scores = np.dot(hotelsMatrix, self.topicWeights)
 
     def getNBestScorers(self, n):
         indeces = np.argsort(-self.scores)[:n]
-        return [HotelFinder.hotelIds[i] for i in indeces]
+        return [hotelsID[i] for i in indeces]
---
+++ main.py
@@ -0,0 +1,4 @@
+from hotelfinder import HotelFinder
+
+a = HotelFinder("I want to go in vacation in the mountains to do some ski")
+print(a.getNBestScorers(5))
