7cdc6017bd0ef48a8f9e9252b9f7ccdb79a2ef4c
SaturneV <75544574+SaturneV@users.noreply.github.com>
2023-09-17 02:15:12 +0200

    fixed ski thing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from hotelfinder import HotelFinder
 
-a = HotelFinder("I want to go in vacation in the mountains to do some ski")
+a = HotelFinder("I want to go in vacation to do some ski")
 print(a.getNBestScorers(5))
